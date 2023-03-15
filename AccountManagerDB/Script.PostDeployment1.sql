16d07025b96de63b4e3bdc182218004fb87efa33
Josh Birnbaum <joshbirnbaum107@gmail.com>
2023-03-15 16:02:15 -0400

    Some UI updates (colors and whatnot).

--- AccountManagerDB/Script.PostDeployment1.sql
+++ AccountManagerDB/Script.PostDeployment1.sql
@@ -1,7 +1,1 @@
-﻿--if not exists (select 1 from dbo.[Account])
---begin
---	insert into dbo.[Account] (Email, UserName, Password, AccountName)
---	values ('joshbirnbaum107@gmail.com', 'joshbirnbaum107@gmail.com', 'password123', 'Google'),
---	('jtbirnbaum@bsu.edu', 'jtbirnbaum', 'test1234', 'Ball State'),
---	('joshbirnbaum107@gmail.com', 'jtbirnbaum', 'instatest1234', 'Instagram');
---end
+﻿
